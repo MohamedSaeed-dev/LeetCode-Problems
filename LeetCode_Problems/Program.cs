93ffbe02c0126b523864bc30f6677a8e58cea460
MohamedSaeed-dev <mohamedsas966@gmail.com>
2024-08-31 17:02:16 +0300

    edit program.cs

--- LeetCode_Problems/Program.cs
+++ LeetCode_Problems/Program.cs
@@ -2,5 +2,5 @@
 using LeetCode_Problems;
 using static LeetCode_Problems.LeetCode;
 
-var x = MinOperations(new string[] { "d1/", "../", "../", "../" });
+var x = MinOperations(new string[] { "d1/", "d2/", "./", "d3/", "../", "d31/" });
 Console.WriteLine(x);
