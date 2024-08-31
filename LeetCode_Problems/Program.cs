a61152eb8c40b079498ddde1e81839f758241240
MohamedSaeed-dev <mohamedsas966@gmail.com>
2024-08-31 15:17:56 +0300

    edit program.cs

--- LeetCode_Problems/Program.cs
+++ LeetCode_Problems/Program.cs
@@ -2,5 +2,5 @@
 using LeetCode_Problems;
 using static LeetCode_Problems.LeetCode;
 
-var x = MaxNumberOfBalloons("nlaeblkon");
+var x = CalPoints(new string[] { "5", "-2", "4", "C", "D", "9", "+", "+" });
 Console.WriteLine(x);
