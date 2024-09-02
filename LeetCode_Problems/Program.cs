de68a1b416759e5cad7b20515ffae6b2812d901e
MohamedSaeed-dev <mohamedsas966@gmail.com>
2024-09-02 11:35:31 +0300

    edit program.cs

--- LeetCode_Problems/Program.cs
+++ LeetCode_Problems/Program.cs
@@ -6,5 +6,5 @@ using static LeetCode_Problems.LeetCode;
 //{
 //    Console.Write(i + " ");
 //}
-var x = ChalkReplacer(new int[] { 5, 1, 5 }, 22);
+var x = DaysBetweenDates("2020-01-15", "2019-12-31");
 Console.WriteLine(x);
